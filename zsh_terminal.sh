524d447ad8c6e82e2f6a8cc8f6aad548b3f52a12
Gaejuk2 <Krkdgud9@gmail.com>
2019-08-09 17:32:09 +0900

    [Mod:zsh_terminal.sh] add zsh-syntax-highlighting

--- zsh_terminal.sh
+++ zsh_terminal.sh
@@ -6,7 +6,14 @@ sudo chsh -s /usr/bin/zsh
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# copy customized configuration file
 cp src/bash_aliases ~/.bash_aliases
 cp src/zshrc ~/.zshrc
 cp src/gaejuk.zsh-theme ~/.oh-my-zsh/themes/
+
+# Apply changed setting
 source ~/.zshrc
+
+# zsh-syntax-highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
