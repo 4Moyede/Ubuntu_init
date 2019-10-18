dce30c428ef116b303ea7bb7cc812f1b24b420e0
Gaejuk2 <krkdgud9@gmail.com>
2019-10-18 20:27:38 +0900

    [Add]:Chrome 설치
    Chrome.sh
    
    Chrome 설치 Shell Script 추가

---
+++ chrome.sh
@@ -0,0 +1,12 @@
+
+# Authorization Key
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# Add PPA to sources.list
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+sudo apt update
+sudo apt install google-chrome-stable
+
+sudo rm -rf /etc/apt/sources.list.d/google.list
+sudo apt autoclean
