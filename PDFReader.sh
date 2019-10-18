93b95cbe47ee32fe023e761995e1906288dc38c5
Gaejuk2 <krkdgud9@gmail.com>
2019-10-18 20:45:32 +0900

    [Ref]:설치파일을 직접 홈페이지에서 다운로드하도록 변경
    tar/FoxitReader*.tar.gz
    PDFReader.sh
    
    tar 폴더에 있던 설치파일 삭제
    직접 홈페이지에서 wget을 통해 다운로드 받도록 수정

--- PDFReader.sh
+++ PDFReader.sh
@@ -1,3 +1,10 @@
 
-tar -xvzf ./tar/FoxitReader.enu.setup.2.4.4.0911.x64.run.tar.gzr
-./tar/FoxitReader.enu.setup.2.4.4.0911.x64.run.tar.gz/FoxitReader.enu.setup.2.4.4.0911.x64.run
+cd tar
+
+# Download FoxitReader Install file
+wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.1/en_us/FoxitReader2.1.0805_Server_x86_enu_Setup.run.tar.gz
+tar -xzvf FoxitReader*.tar.gz
+
+# Install FoxitReader
+sudo chmod a+x FoxitReader*.run
+./FoxitReader*.run
