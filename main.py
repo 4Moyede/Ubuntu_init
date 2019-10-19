beeab96096bf282fdc1fd64bbc7d57ef53f4d03a
Gaejuk2 <krkdgud9@gmail.com>
2019-10-19 15:25:09 +0900

    [Add]:Shell Script 실행 제어 프로그램 추가
    sh/*.sh
    main.py
    
    어떤 프로그램을 설치할지 선택하면 그에 맞는 Shell Script를 실행
    Shell Script 파일들은 모두 sh 디렉토리를 만들어 이동

---
+++ main.py
@@ -0,0 +1,51 @@
+
+import os
+
+
+install_list = [
+    # Name, Detail, Check, File
+    ["ZSH", "Z Unix Shell", " ", "zsh_terminial.sh"],
+    ["neoVIM", "Hyperextensible Vim-based Text Editor", " ", "neovim_idle.sh"],
+    ["GUI", "Gnome, Dash to dock, Tweak", " ", "gnome.sh"],
+    ["UIM", "for hangul", " ", "hangul_uim.sh"],
+    ["VSCode", "Visual Studio Code", " ", "vscode.sh"],
+    ["Chrome", "Google Chrome Web Browser", " ", "chrome.sh"],
+    ["Foxit Reader", "Foxit PDF Reader", " ", "PDFReader.sh"],
+    ["Slack", "Cloud-based Proprietary Instant Messaging Platform", " ", "slack.sh"],
+]
+
+
+def user_choice():
+    choice = 0
+    done = len(install_list)
+
+    while choice is not done:
+        os.system('clear')
+        print("\n\n\t Install list")
+        print("\t----------------------------------------")
+        for p in range(len(install_list)):
+            print("\t [%s] %d. %s : %s" % (install_list[p][2], p+1, install_list[p][0], install_list[p][1]))
+        print("\t [ ] %d. Done. Start Install" % (done + 1))
+        print("\t----------------------------------------")
+        choice = int(input("\t--> ")) - 1
+
+        if 0 <= choice < done:
+            if install_list[choice][2] is ' ':
+                install_list[choice][2] = 'x'
+            elif install_list[choice][2] is 'x':
+                install_list[choice][2] = ' '
+
+
+def start_install():
+    os.system('mkdir tar')
+    os.system('clear')
+    for p in install_list:
+        if p[2] is 'x':
+            print(" Install " + p[0] + "...")
+            print("---------------------------------------------------")
+            os.system('./sh/' + p[3])
+            print("---------------------------------------------------")
+
+
+user_choice()
+start_install()
--- sh/gnome.sh
+++ sh/gnome.sh
@@ -3,10 +3,9 @@
 sudo apt install gnome-session
 
 # Dash to Dock
-wget https://extensions.gnome.org/review/download/8039.shell-extension.zip
-mv 8039.shell-extension.zip dash-to-dock.zip
+wget -O ./tar/dash-to-dock.zip https://extensions.gnome.org/review/download/8039.shell-extension.zip
 mkdir -p ~/.local/share/gnome-shell/extensions/
-unzip dash-to-dock@micxgx.gmail.com.zip -d ~/.local/share/gnome-shell/extensions/dash-to-dock.zip
+unzip ./tar/dash-to-dock.zip -d ~/.local/share/gnome-shell/extensions/dash-to-dock.zip
 gnome-shell --replace
 
 # Tweak Tool
