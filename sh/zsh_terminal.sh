7158dc437b96e4aac3f8d9505c1757e2d178d0c2
Gaejuk2 <Krkdgud9@gmail.com>
2019-12-02 01:53:08 +0900

    [Bug]:zsh, oh-my-zsh 설치 오류
    sh/zsh_terminal.sh
    src/gaejuk_theme
    src/zshrc
    
    oh-my-zsh 설치 전 환경변수 등록 추가, 같은 컴퓨터 내에 다른 사용자가 oh-my-zsh 사용중일 경우 설치가 되지 않음.
    zsh-autosuggestions plugin 추가.

--- sh/zsh_terminal.sh
+++ sh/zsh_terminal.sh
@@ -1,19 +1,21 @@
 
 # install zsh
-sudo apt-get install zsh
+sudo apt install zsh
 sudo chsh -s /usr/bin/zsh
 
 # install oh-my-zsh
+export ZSH='$HOME/.oh-my-zsh'
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # copy customized configuration file
 cp src/bash_aliases ~/.bash_aliases
 cp src/zshrc ~/.zshrc
-cp src/gaejuk.zsh-theme ~/.oh-my-zsh/themes/
+cp src/gaejuk_theme ~/.oh-my-zsh/themes/gaejuk.zsh-theme
+
+# install zsh plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Apply changed setting
 source ~/.zshrc
 
-# zsh-syntax-highlight
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
